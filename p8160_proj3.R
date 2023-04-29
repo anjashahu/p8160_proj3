bb28f4664420a979bb7f147304700b09b3a7292c
Ting-Hsuan Chang <65819949+tinghsuan-chang@users.noreply.github.com>
2023-04-29 14:36:56 -0400

    Add files via upload

--- p8160_proj3.R
+++ p8160_proj3.R
@@ -1,11 +1,24 @@
 library(tidyverse)
 library(ggplot2)
+library(lme4)
 
 ## prepare data, add log(infection_rate)
 data <- read.csv("covid_working_data.csv")
 data <- data %>%
   mutate(week_start = as.Date(week_start),
+         week = week(week_start),
          log_infection_rate = log(infection_rate))
+
+## fit lmer 
+fit_lmer <- lmer(
+  log_infection_rate ~ retail_and_recreation_percent_change_from_baseline +
+    parks_percent_change_from_baseline + transit_stations_percent_change_from_baseline +
+    government_response_index + containment_index + economic_support_index +
+    stringency_index + week + population_density + Percentage_over_65 + (1 | state),
+  data = data %>%
+    mutate(log_infection_rate = log(infection_rate)) %>%
+    filter(log_infection_rate != -Inf)
+)
 
 ## log likelihood
 #### OBSERVED DATA:
@@ -23,7 +36,7 @@ data <- data %>%
 loglik <- function(log_infection_rate, x, pop_dens, eld_pop, alpha, beta, gamma, delta, sigma_u, sigma_e) {
   mean <- alpha + x%*%beta + gamma*pop_dens + delta*eld_pop
   var <- sigma_u^2 + sigma_e^2
-  loglik_ij <- -log(2*pi*var)/2 - ((log_infection_rate - mean)^2)/(2*pi*var)
+  loglik_ij <- -log(2*pi*var)/2 - ((log_infection_rate - mean)^2)/(2*var)
   loglik_ij <- loglik_ij[loglik_ij != -Inf] ## remove -Inf values
   return(sum(loglik_ij, na.rm = T))
 }
@@ -51,7 +64,7 @@ MH <- function(log_infection_rate, x, pop_dens, eld_pop, pars, avec) {
     prop <- res
     ## proposed step of i^th element =  current i^th element + U(-avec[i], avec[i]) 
     prop[i] <- res[i] + 2 * avec[i] * (runif(1) - 0.5)
-    if (log(runif(1)) < logpost(log_infection_rate, x, pop_dens, eld_pop, alpha = prop[1], beta = prop[2:8], gamma = prop[9], delta = prop[10], sigma_u = prop[11], sigma_e = prop[12]) - logpost(log_infection_rate, x, pop_dens, eld_pop, alpha = res[1], beta = res[2:8], gamma = res[9], delta = res[10], sigma_u = res[11], sigma_e = res[12]))
+    if (log(runif(1)) < logpost(log_infection_rate, x, pop_dens, eld_pop, alpha = prop[1], beta = prop[2:9], gamma = prop[10], delta = prop[11], sigma_u = prop[12], sigma_e = prop[13]) - logpost(log_infection_rate, x, pop_dens, eld_pop, alpha = res[1], beta = res[2:9], gamma = res[10], delta = res[11], sigma_u = res[12], sigma_e = res[13]))
         res[i] <- prop[i]
   }
   return(res)
@@ -64,28 +77,51 @@ x <- as.matrix(x)
 
 ## run MH algorithm
 set.seed(123)
-nrep <- 90000 
-npars <- 12
-avec <- rep(0.5, npars) 
+nrep <- 100000 
+npars <- 13
+avec <- c(rep(0.01, 10), 0.1, 0.01, 0.01)
 mchain <- matrix(NA, nrow = nrep, ncol = npars)
-mchain[1,] <- rep(1, npars) 
+mchain[1,] <- c(summary(fit_lmer)$coefficients[,1], 0.8213, 0.7826) 
 for (i in 2:nrep) {
   mchain[i,] <- MH(data$log_infection_rate, x, data$population_density, data$Percentage_over_65, mchain[i-1,], avec)
 }
 
 ## save mchain
 mchain <- as.data.frame(mchain) 
-colnames(mchain) <- c("alpha", "beta1", "beta2", "beta3", "beta4", "beta5", "beta6", "beta7", "gamma", "delta", "sigma_u", "sigma_e")
+colnames(mchain) <- c("alpha", "beta1", "beta2", "beta3", "beta4", "beta5", "beta6", "beta7", "week", "gamma", "delta", "sigma_u", "sigma_e")
 #save(mchain, file = "mchain.Rda")
 #load("mchain.Rda")
+mchain_last <- mchain[50001:nrep,]
 
-## plot convergence result
-ggplot(mchain, aes(x = 1:nrow(mchain), y = delta)) +
-  geom_line() +
-  theme_bw()
+## line plots (all)
+par(mfrow = c(3,5))
+for(i in 1:13){
+  plot(x = 1:nrow(mchain), y = mchain[,i], type = "l", lty = 1, main = colnames(mchain)[i])
+  lines(x = 1:nrow(mchain), y = mchain[,i], type = "l", lty = 1)
+}
+
+## line plots (after burnout)
+par(mfrow = c(3,5))
+for(i in 1:13){
+  plot(x = 1:nrow(mchain_last), y = mchain_last[,i], type = "l", lty = 1, main = colnames(mchain_last)[i])
+  lines(x = 1:nrow(mchain_last), y = mchain_last[,i], type = "l", lty = 1)
+}
 
 ## histogram
-ggplot(mchain, aes(x = delta)) +
+ggplot(mchain, aes(x = alpha)) +
   geom_histogram() +
   theme_bw()
+
+## function to investigate if choices of “a” are good
+numunique <- function(mat){
+  for (i in 1:ncol(mat))
+    cat(i, "\t", length(unique(mat[,i])), "\n")
+}
+
+## look at how well “a” works for the different parameters
+numunique(mchain)
+
+
+
+
 
